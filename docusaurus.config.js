4f2cd67c3f1f8033edf263f117cd1d59398fc1f6
Christian Musial <christian.musial1@gmail.com>
2025-04-13 00:25:52 -0400

    changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'cmusial', // Usually your GitHub org/user name.
   projectName: 'scout-docs', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
